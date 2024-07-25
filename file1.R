fafefba16c17498774333a54d39db7bdfea404a9
Kwasi48 <abuwestwood@gmail.com>
2024-07-25 11:57:05 +0000

    breaking for a minute

--- file1.R
+++ file1.R
@@ -8,3 +8,27 @@ flights %>% filter(dest == "IAH") %>%  group_by(year, month, day) %>% summarize(
 flights %>% filter(month == 1 & day == 1)
 flights %>% filter(month %in% c(1, 2))
 view(flights %>% filter(month %in% c(1, 2)))
+
+flights %>%  arrange(desc(dep_delay))
+flights %>%  arrange(desc(month))
+
+flights %>%  distinct()
+
+flights %>% distinct(origin, dest)
+
+flights %>%  distinct(origin, dest, .keep_all = TRUE)
+
+flights %>%  count(origin, dest, sort = TRUE)
+
+#Excercise
+esex <- flights %>% filter(dep_delay >= 120) %>%  filter(dest  == "IAH" | dest == "HOU") %>%  filter( carrier == 'UA' | carrier == 'AA' | carrier == "DL")  %>% filter(month == 7 | month == 8 | month == 9)
+esex1 <- esex %>% filter(arr_delay > 2 ) %>%  filter(sched_dep_time >= dep_time) 
+esex1
+#esex2 <- esex1 %>%  filter(dep_delay == -1)
+#esex2
+
+flights %>% arrange(desc(distance))
+flights %>% arrange(distance)
+dayss <- flights %>% distinct(day)
+view(dayss)
+
